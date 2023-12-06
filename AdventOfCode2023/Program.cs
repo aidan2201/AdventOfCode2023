1da3c39376cdaa92e52be3a9fa3592c1c77c657d
aidan2201 <aidan.h2201@gmail.com>
2023-12-06 22:51:28 +0000

    day 2 puzzle 1 solved

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,5 +1,9 @@
 ﻿using AdventOfCode2023;
 
+Console.WriteLine("Day 1");
 Console.WriteLine(Day1.Puzzle1Solution());
 Console.WriteLine(Day1.Puzzle2Solution());
+Console.WriteLine();
+Console.WriteLine("Day 2");
+Console.WriteLine(Day2.Puzzle1Solution());
 Console.ReadLine();
